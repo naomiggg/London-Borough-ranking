8a8bdb41a608582bcd13abddf1bf2cd05a3ea6fe
Naomi Guo <36001487+naomiggg@users.noreply.github.com>
2018-04-09 23:40:17 +0100

    -

--- Script.js
+++ Script.js
@@ -1,11 +1,26 @@
 $(document).ready(function() {
 
-    $.get('boroughs.json', function(data) {
+    var select = $('<select><option>Choose Area</option></select>').appendTo('.selection');
 
-        var select = $('<select>').appendTo('.selection');
+    $.get('boroughs.json', function(data) {
 
         $.each(data, function(index, item) {
             select.append($('<option>').attr('value', item.area).text(item.area));
+        });
+
+        select.change(function(e) {
+            var area = data.find(item => item['area'] === this.value);
+
+            var area_info = [
+                '<div>Area Name: ' + area['area'] + '</div>',
+                '<div>Population: ' + area['population'] + '</div>',
+                '<div>Bars and Pubs: ' + area['bars-and-pubs'] + '</div>',
+                '<div>Crime: ' + area['crime'] + '</div>',
+                '<div>Crime per Population: ' + area['crime-per-pop'] + '</div>',
+                '<div>Rent: ' + area['rent'] + '</div>',
+            ];
+
+            $('.area-details').html(area_info);
         });
 
     });

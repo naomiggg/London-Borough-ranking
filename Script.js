d86cb58f5734135cd63c2719c8d1023fb1838eab
Naomi Guo <36001487+naomiggg@users.noreply.github.com>
2018-04-10 22:57:53 +0100

    -

--- Script.js
+++ Script.js
@@ -29,13 +29,13 @@ $(document).ready(function() {
 
     var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
-    accessToken: 'your.mapbox.access.token'
+    accessToken: 'pk.eyJ1IjoibmFvbWlnIiwiYSI6ImNqZnU3ZmV5NjAxb3oycG1hbzllN2x5dmIifQ.pXBlEaoKwx_yPBesCr5X_A'
 }).addTo(mymap);
+
 
 });
 
